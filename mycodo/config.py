ffbc39ea9164c683deff756eb26bb173cb54c18c
Ömer Faruk <o.faduzce@gmail.com>
2025-03-20 23:14:51 +0300

    Additional changes for v8.16.1

--- mycodo/config.py
+++ mycodo/config.py
@@ -135,7 +135,7 @@ LOGIN_BAN_SECONDS = 600  # 10 minutes
 # Check for upgrade every 2 days (if enabled)
 UPGRADE_CHECK_INTERVAL = 172800
 
-TAGS_URL = 'https://api.github.com/repos/kizniche/Mycodo/git/refs/tags'
+TAGS_URL = 'https://api.github.com/repos/ofaydn/Mycodo/git/refs/tags'
 
 LANGUAGES = {
     'en': 'English',
